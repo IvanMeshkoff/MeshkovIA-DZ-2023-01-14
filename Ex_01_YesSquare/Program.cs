72cd5dc1ecbb0f91aa481d88632a25c9104b4209
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-21 14:06:41 +0300

    Example 01 Yes ore Not Square

---
+++ Ex_01_YesSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пример №1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else 
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
