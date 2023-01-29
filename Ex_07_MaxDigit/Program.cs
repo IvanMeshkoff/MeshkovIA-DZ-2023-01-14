bfd5fb9a36d8dfcf8b612669953d479daae4b588
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-29 21:07:33 +0300

    Ex_07 added

---
+++ Ex_07_MaxDigit/Program.cs
@@ -0,0 +1,35 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+// Вариант №1:
+
+Console.Clear();
+int number1 = new Random().Next(10, 100);
+string a = Convert.ToString(number1);
+Console.WriteLine($"Дано произвольное число: {number1}");
+
+if (a[0]>a[1])
+{
+    Console.WriteLine($"Наибольшая цифра числа будет равна: {a[0]}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа будет равна: {a[1]}");
+}
+Console.WriteLine();
+
+// Вариант №2:
+
+int number2 = new Random().Next(10, 100);
+Console.WriteLine($"Дано произвольное число: {number2}");
+
+int a1 = number2 / 10;
+int a2 = number2 % 10;
+
+if (a1>a2)
+{
+    Console.WriteLine($"Наибольшая цифра числа будет равна: {a1}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа будет равна: {a2}");
+}
