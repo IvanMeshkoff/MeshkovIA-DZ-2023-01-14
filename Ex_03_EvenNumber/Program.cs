4014d14bc45166ea73dc1b8818ab69d285997143
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-29 18:23:36 +0300

    Ex_03 added

---
+++ Ex_03_EvenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+// Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является ЧЕТНЫМ!");
+}
+else
+{
+    Console.WriteLine("Число {0} является НЕЧЕТНЫМ!", number);
+}
