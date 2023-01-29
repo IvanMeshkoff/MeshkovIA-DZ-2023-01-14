aced85cf86ae37366db5b3e8e8ebb672a1a63d0c
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-29 19:51:38 +0300

    Ex_05 added

---
+++ Ex_05_LastDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать последнюю цифру трёхзначного числа
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int result1 = number % 100;
+int result2 = result1 % 10;
+
+Console.WriteLine(result2);
