36ba2a618f32755f96a40c33dbccf3a118c90ada
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-02-01 21:18:08 +0300

    Ex_10 added

---
+++ Ex_10_ThirdDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number >= 1000)
+{
+  while (number >= 1000)
+  {
+    number = number / 10;
+  }
+  int third = number % 10;
+  Console.WriteLine($"Третья цифра числа будет = {third}");
+}
+else
+{
+Console.WriteLine($"В числе {number} третьей цифры не существует!");
+}
